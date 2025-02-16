df9db0d139a77b7cc111eb7eca8550405721945f
Erbol Zhaparov <ae.270718@gmail.com>
2025-02-17 00:26:33 +0600

    finished extra

--- js36-lab/js36-lab-extra/js36-extra.js
+++ js36-lab/js36-lab-extra/js36-extra.js
@@ -1,8 +1,26 @@
 function showTab() {
   const tabs = document.querySelectorAll('.item');
-  const content = document.querySelectorAll('.content-item');
+  const content = document.querySelectorAll('.tabs_item');
   
-  
+  tabs.forEach((item) => {
+    item.addEventListener('click', () => {
+      tabs.forEach((item) => {
+        item.classList.remove('active')
+      })
+
+      item.classList.add('active')
+    })
+  })
+
+  content.forEach((item, index) => {
+    tabs[index].addEventListener('click', () => {
+      content.forEach((item) => {
+        item.classList.remove('active')
+      })
+
+      item.classList.add('active')
+    })
+  })
 }
 
 showTab();
