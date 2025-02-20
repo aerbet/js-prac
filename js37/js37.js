04c893ad0ed0e4c00d2ed090136353d301ae6e65
Erbol Zhaparov <ae.270718@gmail.com>
2025-02-20 18:02:09 +0600

    starting second task of 37

--- js37/js37.js
+++ js37/js37.js
@@ -1,22 +1,40 @@
-const textArea = document.querySelector('#textarea');
-const saveBtn = document.querySelector('#save');
-const clearBtn = document.querySelector('#clear');
+function openText () {
+    document.querySelector('#textBlock').style.display = 'block';
+    const textArea = document.querySelector('#textarea');
+    const saveBtn = document.querySelector('#save');
+    const clearBtn = document.querySelector('#clear');
 
-saveBtn.addEventListener('click', (e) => {
-    localStorage.setItem('text', textArea.value);
-    console.log(textArea.value)
-})
+    saveBtn.addEventListener('click', (e) => {
+        localStorage.setItem('text', textArea.value);
+        const table = document.createElement('li');
+        table.innerHTML = `${textArea.value}`;
+        document.body.appendChild(table);
+    })
 
-if (textArea.value !== null) {
-    textArea.value = `${localStorage.getItem('text')}`;
+    if (textArea.value !== null) {
+        textArea.value = `${localStorage.getItem('text')}`;
+    }
+
+    if (textArea.value === 'null') {
+        textArea.value = '';
+    }
+
+    clearBtn.addEventListener('click', (e) => {
+        localStorage.removeItem('text');
+        textArea.value = '';
+        localStorage.clear();
+    })
 }
 
-if (textArea.value === 'null') {
-    textArea.value = '';
-}
+const userName = document.querySelector('#login');
+const loginBtn = document.querySelector('#loginBtn');
 
-clearBtn.addEventListener('click', (e) => {
-    localStorage.removeItem('text');
-    textArea.value = '';
-    localStorage.clear();
+loginBtn.addEventListener('click', (e) => {
+    if (userName.value !== null && userName.value !== '')  {
+
+        localStorage.setItem('user', userName.value);
+        if (localStorage.getItem('user') === '123') {
+            openText();
+        }
+    }
 })
