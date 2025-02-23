e1fefbcc95de5ab1a73a1345f2081d7bb46d1ff7
Erbol Zhaparov <ae.270718@gmail.com>
2025-02-23 15:24:40 +0600

    finished 37 task

--- js37/js37.js
+++ js37/js37.js
@@ -1,4 +1,5 @@
 const textBlock = document.querySelector('#textBlock');
+const userBlock = document.querySelector('#userBlock');
 
 function openText () {
     setTimeout(() => {
@@ -11,13 +12,14 @@ function openText () {
     const textArea = document.querySelector('#textarea');
     const saveBtn = document.querySelector('#save');
     const clearBtn = document.querySelector('#clear');
+    const exitBtn = document.querySelector('#exit');
 
     saveBtn.addEventListener('click', () => {
-        localStorage.setItem('text', textArea.value);
+        localStorage.setItem(`${userName.value}`, textArea.value);
     })
 
     if (textArea.value !== null) {
-        textArea.value = `${localStorage.getItem('text')}`;
+        textArea.value = `${localStorage.getItem(`${userName.value}`)}`;
     }
 
     if (textArea.value === 'null') {
@@ -27,6 +29,20 @@ function openText () {
     clearBtn.addEventListener('click', () => {
         localStorage.removeItem('text');
         textArea.value = '';
+    })
+    
+    exitBtn.addEventListener('click', () => {
+        if (textArea.value !== '' && textArea.value !== null)  {
+            localStorage.setItem(`${userName.value}`, textArea.value);
+        }
+        textBlock.style.visibility = 'none';
+        textBlock.style.pointerEvents = 'none';
+        textBlock.style.opacity = '0';
+        textBlock.style.transform = 'translate(0%, 22%) scale(0.8)';
+        userBlock.style.visibility = 'visible';
+        userBlock.style.pointerEvents = 'visible';
+        userBlock.style.opacity = '1';
+        userBlock.style.transform = 'translate(0%, 0%) scale(1)';
     })
 }
 
@@ -47,6 +63,10 @@ loginBtn.addEventListener('click', () => {
 
     if (userInfo.includes(userName.value)) {
         openText();
+        userBlock.style.visibility = 'none';
+        userBlock.style.pointerEvents = 'none';
+        userBlock.style.opacity = '0';
+        userBlock.style.transform = 'translate(0%, 22%) scale(0.8)';
     } else {
         textBlock.style.visibility = 'none';
         textBlock.style.pointerEvents = 'none';
@@ -54,3 +74,4 @@ loginBtn.addEventListener('click', () => {
         textBlock.style.transform = 'translate(0%, 22%) scale(0.8)';
     }
 })
+
