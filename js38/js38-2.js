a2d9d274e88ea43b3c023fb581edce2b129ac392
Erbol Zhaparov <ae.270718@gmail.com>
2025-02-28 00:16:06 +0600

    finished second task of js38

--- js38/js38-2.js
+++ js38/js38-2.js
@@ -1,42 +1,47 @@
-const tableName = document.querySelector('#tableName');
-const tableCapital = document.querySelector('#tableCapital');
-const tableFlag = document.querySelector('#tableFlag');
-const tablePop = document.querySelector('#tablePop');
-const tableCode = document.querySelector('#tableCode');
+const tableBody = document.querySelector('#tableBody');
+const tableBtn = document.querySelector('#tableShow');
 
-const request = config => {
-    const xhr = new XMLHttpRequest();
+tableBtn.addEventListener('click', () => {
 
-    xhr.addEventListener('load', function () {
-        const response = JSON.parse(xhr.responseText);
-        console.log(response);
-        if (this.status >= 200 && this.status < 300) {
-            response.forEach(el => tableName.innerHTML = el.name);
-            response.forEach(el => tableCapital.innerHTML = el.capital);
-            response.forEach(el => tableCode.innerHTML = el.alpha2Code);
-            response.forEach(el => tablePop.innerHTML = el.population);
-            response.forEach(el => tableFlag.innerHTML = `<img src="${el.flag}" alt="flag">`);
-        } else {
-            config.error(this.status)
-        }
-    });
+    const request = config => {
+        const xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('error', () => {
-        console.log('No Internet');
+        xhr.addEventListener('load', function () {
+            const response = JSON.parse(xhr.responseText);
+
+            if (this.status >= 200 && this.status < 300) {
+                for (let i = 0; i < response.length; i ++) {
+                    const country = response[i];
+                    const tableContent = document.createElement('tr');
+                    tableContent.classList.add('tableContent');
+
+                    tableContent.innerHTML = `
+                    <td class="tableCode">${country.alpha2Code}</td>
+                    <td class="tableFlag"><img style="width: 30px; height: 20px;" src="${country.flag}" alt="flag"></td>
+                    <td class="tableName">${country.name}</td>
+                    <td class="tableCapital">${country.capital}</td>
+                    <td class="tablePop">${country.population}</td>
+                    `;
+
+                    tableBody.appendChild(tableContent);
+                }
+            } else {
+                config.error(this.status)
+            }
+        });
+
+        xhr.addEventListener('error', () => console.log('No Internet'));
+        xhr.addEventListener('timeout', () => console.log('Timeout'));
+
+        xhr.open(config.method, config.url);
+        xhr.send();
+    }
+
+    request({
+        method: 'GET',
+        url: `https://restcountries.com/v2/all?fields=alpha2Code,flag,name,capital,population`,
+        error: message => {
+            alert(`Error ${message}`);
+        },
     })
-
-    xhr.addEventListener('timeout', () => {
-        console.log('Timeout');
-    })
-
-    xhr.open(config.method, config.url);
-    xhr.send();
-}
-
-request({
-    method: 'GET',
-    url: `https://restcountries.com/v2/name/Kyrgyzstan`,
-    error: message => {
-        alert(`Error ${message}`);
-    },
 })
