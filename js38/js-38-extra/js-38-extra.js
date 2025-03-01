a967ead91f81c8555016e2e75766ea1d6ff2d447
Erbol Zhaparov <ae.270718@gmail.com>
2025-03-01 13:48:09 +0600

    searching styles for todo list

--- js38/js-38-extra/js-38-extra.js
+++ js38/js-38-extra/js-38-extra.js
@@ -5,26 +5,35 @@ const request = config => {
 
     xhr.addEventListener('load', function () {
         const response = JSON.parse(xhr.responseText);
-
+        
+        
         if (this.status >= 200 && this.status < 300) {
-            for (let i = 0; i < response.length - 199; i++) {
+            for (let i = 0; i < response.length - 197; i++) {
                 const todos = response[i];
                 const todosList = document.createElement('div');
-
+                todosList.classList.add('todosBody');
+                
                 todosList.innerHTML = `
-                    <div class="id">id: ${todos.id}</div>
-                    <div class="userTitle">title: ${todos.title}</div>
-                    <div class="userStatus">status: ${todos.completed}</div>
+                    <div class="todosItems id">id: ${todos.id}</div>
+                    <div class="todosItems userTitle">title: ${todos.title}</div>
+                    <div class="todosItems userStatus">status: ${todos.completed}</div>
+                    <button id="todoBtn">Complete</button>
                     `;
-
+                
                 todoBody.appendChild(todosList);
+                
             }
-
             const btn = document.querySelector('#todoBtn');
-            btn.addEventListener('click', () => {
-                document.querySelector('.userStatus').style.border = '3px solid green';
-                document.querySelector('.userStatus').innerHTML = 'status: true';
-            })
+            for (let i = 0; i < response.length; i++) {
+                const status = response[i];
+                if (status.completed === true) {
+                    btn.addEventListener('click', () => {
+                        status.completed = true;
+                        document.querySelector('.userStatus').style.border = '3px solid green';
+                    })
+                }
+            }
+            
         } else {
             console.log(this.errorCode)
         }
