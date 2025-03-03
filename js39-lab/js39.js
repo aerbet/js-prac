a8d29fb68d387b73461b20b62dc6862bab0f4742
Erbol Zhaparov <ae.270718@gmail.com>
2025-03-03 12:58:08 +0600

    working on user interface

--- js39-lab/js39.js
+++ js39-lab/js39.js
@@ -1,8 +1,22 @@
+const content = document.querySelector('.content');
+const input = document.querySelector('#textInput');
 const date = document.querySelector('#dateInput');
 const select = document.querySelector('#select');
 const btn = document.querySelector('#saveBtn');
+let postId = 1;
 
 btn.addEventListener('click', () => {
   console.log(date.value)
   console.log(select.value)
+
+  const contentItem = document.createElement('div');
+  contentItem.classList.add('content__item');
+
+  contentItem.innerHTML = `
+              <div class="contentItems">Post №${postId++}</div>
+              <div class="contentItems">at ${date.value.split('')}</div>
+              <div class="contentItems">being in: ${select.value}</div>
+              `;
+
+  content.appendChild(contentItem);
 });
