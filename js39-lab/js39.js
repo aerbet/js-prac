b2ea32ab25c66d3b50705a372add33ad9fd74768
Erbol Zhaparov <ae.270718@gmail.com>
2025-03-04 23:07:20 +0600

    some shitty code, will try tomorrow with fresh head

--- js39-lab/js39.js
+++ js39-lab/js39.js
@@ -1,22 +1,68 @@
 const content = document.querySelector('.content');
 const input = document.querySelector('#textInput');
 const date = document.querySelector('#dateInput');
-const select = document.querySelector('#select');
 const btn = document.querySelector('#saveBtn');
 let postId = 1;
 
-btn.addEventListener('click', () => {
-  console.log(date.value)
-  console.log(select.value)
-
-  const contentItem = document.createElement('div');
-  contentItem.classList.add('content__item');
-
-  contentItem.innerHTML = `
-              <div class="contentItems">Post №${postId++}</div>
-              <div class="contentItems">at ${date.value.split('')}</div>
-              <div class="contentItems">being in: ${select.value}</div>
+const request = config => {
+  const xhr = new XMLHttpRequest();
+  
+  xhr.addEventListener('load', function () {
+    const response = JSON.parse(xhr.responseText);
+    console.log(response.length)
+    for (let i = 0; i < response.length; i++) {
+      let country = response[i];
+      const select = document.createElement('select');
+      
+      content.innerHTML = `
+        <option>${country.flag}</option>
+      `
+    }
+    
+    if (this.status >= 200 && this.status < 300) {
+      
+      
+      
+      btn.addEventListener('click', () => {
+        const contentItem = document.createElement('div');
+        contentItem.classList.add('content__item');
+        
+        contentItem.innerHTML = `
+              <div class="contentHeader">
+                <div class="contentItems">Post <b>№${postId++}</b></div>
+                <div class="contentItems">at <b>${date.value.split('-').reverse().join('.')}</b></div>
+                <div class="contentItems flagContent">being in: <b style="margin-left: 5px">${select.value}</b><div class="flag"></div></div>
+              </div>
+              <div class="contentBody">
+                <div class="contentItems">${input.value}</div>
+              </div>
               `;
+        
+        content.appendChild(contentItem);
+        
+        response.forEach(el => document.querySelector('.flag').innerHTML = `<img class="flagImg" src="${el.flag}" alt="flag">`);
+      });
+    } else {
+      config.error(this.status)
+    }
+  });
+  
+  xhr.addEventListener('error', () => {
+    console.log('No Internet');
+  })
+  
+  xhr.addEventListener('timeout', () => {
+    console.log('Timeout');
+  })
+  
+  xhr.open(config.method, config.url);
+  xhr.send();
+}
 
-  content.appendChild(contentItem);
-});
+request({
+  method: 'GET',
+  url: `https://restcountries.com/v2/all?fields=name,flag`,
+  error: message => {
+    alert(`Error ${message}`);
+  },
+})
